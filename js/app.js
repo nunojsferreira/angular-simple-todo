517f8ba35586a46e609568cb5bd07f0420380e7f
Nuno Ferreira <nunojsferreira@gmail.com>
2015-10-17 20:01:09 +0100

    Added controller and add and delete actions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 "use strict"
+
+angular.module('todo', [])
+
+.controller('ToDoCtrl', ['$scope', function($scope) {
+    $scope.tasklist = [];
+
+    $scope.addTask = function() {
+        if(event.keyCode == 13 && $scope.taskName) {
+            $scope.tasklist.push({"name": $scope.taskName});
+            $scope.taskName = "";
+        }
+    };
+
+    $scope.deleteTask = function() {
+
+    };
+
+}]);
